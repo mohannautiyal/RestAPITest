bf0099f5df885a9c090280e78055d0ac7be4edcf
mohannautiyal <mohannautiyal80@gmail.com>
2021-01-30 12:29:13 +0530

    Tests

---
+++ src/test/java/com/home/RestAppTests.java
@@ -0,0 +1,28 @@
+package com.home;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class RestAppTests {
+	
+	
+	@Test
+	public void TestAPI() 
+	{
+	  
+		RestAssured.baseURI ="http://localhost:8085/RestApplication-0.1";	
+		RequestSpecification rs = RestAssured.given().queryParam("a", 10)
+				.queryParam("b", 20);
+		
+		Response resp = rs.get("/calculator/add");
+		int status =resp.getStatusCode();
+		
+		Assert.assertTrue(status==200, "Service failed");
+		
+	}
+
+}
